985abc1fb4a90f769b67b871a9eb4fcc5971feee
binxu <binxu.wang@wustl.edu>
2019-06-15 12:47:09 -0500

    add monkey code

--- process_monkey.py
+++ process_monkey.py
@@ -26,34 +26,34 @@ test_recordings = [('city', '2011_09_26_drive_0104_sync'), ('residential', '2011
 if not os.path.exists(DATA_DIR): os.mkdir(DATA_DIR)
 
 # Download raw zip files by scraping KITTI website
-def download_data():
-    base_dir = os.path.join(DATA_DIR, 'raw/')
-    if not os.path.exists(base_dir): os.mkdir(base_dir)
-    for c in categories:
-        url = "http://www.cvlibs.net/datasets/kitti/raw_data.php?type=" + c
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content)
-        drive_list = soup.find_all("h3")
-        drive_list = [d.text[:d.text.find(' ')] for d in drive_list]
-        print( "Downloading set: " + c)
-        c_dir = base_dir + c + '/'
-        if not os.path.exists(c_dir): os.mkdir(c_dir)
-        for i, d in enumerate(drive_list):
-            print( str(i+1) + '/' + str(len(drive_list)) + ": " + d)
-            url = "https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/" + d + "/" + d + "_sync.zip"
-            urllib.request.urlretrieve(url, filename=c_dir + d + "_sync.zip")
-
-
-# unzip images
-def extract_data():
-    for c in categories:
-        c_dir = os.path.join(DATA_DIR, 'raw/', c + '/')
-        zip_files = list(os.walk(c_dir, topdown=False))[-1][-1]#.next()
-        for f in zip_files:
-            print( 'unpacking: ' + f)
-            spec_folder = f[:10] + '/' + f[:-4] + '/image_03/data*'
-            command = 'unzip -qq ' + c_dir + f + ' ' + spec_folder + ' -d ' + c_dir + f[:-4]
-            os.system(command)
+# def download_data():
+#     base_dir = os.path.join(DATA_DIR, 'raw/')
+#     if not os.path.exists(base_dir): os.mkdir(base_dir)
+#     for c in categories:
+#         url = "http://www.cvlibs.net/datasets/kitti/raw_data.php?type=" + c
+#         r = requests.get(url)
+#         soup = BeautifulSoup(r.content)
+#         drive_list = soup.find_all("h3")
+#         drive_list = [d.text[:d.text.find(' ')] for d in drive_list]
+#         print( "Downloading set: " + c)
+#         c_dir = base_dir + c + '/'
+#         if not os.path.exists(c_dir): os.mkdir(c_dir)
+#         for i, d in enumerate(drive_list):
+#             print( str(i+1) + '/' + str(len(drive_list)) + ": " + d)
+#             url = "https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/" + d + "/" + d + "_sync.zip"
+#             urllib.request.urlretrieve(url, filename=c_dir + d + "_sync.zip")
+#
+#
+# # unzip images
+# def extract_data():
+#     for c in categories:
+#         c_dir = os.path.join(DATA_DIR, 'raw/', c + '/')
+#         zip_files = list(os.walk(c_dir, topdown=False))[-1][-1]#.next()
+#         for f in zip_files:
+#             print( 'unpacking: ' + f)
+#             spec_folder = f[:10] + '/' + f[:-4] + '/image_03/data*'
+#             command = 'unzip -qq ' + c_dir + f + ' ' + spec_folder + ' -d ' + c_dir + f[:-4]
+#             os.system(command)
 
 excluded_list = []
 # Create image datasets.
@@ -64,7 +64,8 @@ def process_data():
     splits['test'] = test_recordings
     not_train = splits['val'] + splits['test']
     # for c in categories:  # Randomly assign recordings to training and testing. Cross-validation done across entire recordings.
-    c_dir = os.path.join(DATA_DIR, 'RAW','/')
+    c_dir = os.path.join(DATA_DIR, 'RAW') # no \
+    seq_clip_list ={}
     folders = os.listdir(c_dir) # list(os.walk(c_dir, topdown=False))[-1][-2]
     for folder in folders:
         if folder in excluded_list:
@@ -79,13 +80,16 @@ def process_data():
         for i, img_id in enumerate(img_ids):
             if img_id == cur_id:
                 cur_id += 1
+                if img_id == img_ids[-1]:
+                    groups.append(((start_i, i + 1), (start_id, cur_id - 1)))
             else:
-                groups.append( ((start_i, i + 1), (start_id, cur_id)) )  # (start_i, end_i + 1), (start_id, end_id)
+                groups.append( ((start_i, i + 1), (start_id, cur_id - 1)) )
+                # (start_i, end_i + 1), (start_id, end_id)
                 # filename[start_i:i+1] = ['start_id', ... 'end_id']
                 start_id = img_id
                 start_i = i + 1
                 cur_id = img_id + 1  # predictive coding!
-
+        seq_clip_list[folder] = groups
         # splits['train'] += [(c, f) for f in folders if (c, f) not in not_train]
     # TODO!
     for split in splits:
@@ -117,6 +121,6 @@ def process_im(im, desired_sz):
 
 
 if __name__ == '__main__':
-    download_data()
-    extract_data()
+    # download_data()
+    # extract_data()
     process_data()
